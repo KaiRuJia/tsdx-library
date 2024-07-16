67a6c1074ea54d0336746d12185338c45cfdc7c6
jkr <jkr301041@163.com>
2024-07-16 17:12:27 +0800

    'init'

--- src/util.ts
+++ src/util.ts
@@ -6,4 +6,23 @@ export function daysBetweenDate(startDate: string, endDate: string) {
   return diffDays
 }
 
-console.log(daysBetweenDate('2024-07-01', '2024-07-08'))
+// console.log(daysBetweenDate('2024-07-01', '2024-07-08'))
+
+export function compose(...funcs: any) {
+  if (funcs.length === 0) return (arg: any) => arg
+  if (funcs.length === 1) return funcs[0]
+  return funcs.reduce((a: any, b: any) => {
+    return  (...args: any) => a(b(...args))
+  })
+}
+
+// const add = (x: number) => {
+//   return x + 1
+// }
+
+// const double = (x: number) => {
+//   return x * 2
+// }
+// const composeFunc = compose(add, double)
+// console.log(composeFunc(3)) // (3 * 2) + 1
+
